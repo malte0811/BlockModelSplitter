0d8644ab5de56b17f31fa91e95c23f9286c94b5f
malte0811 <malte0811@web.de>
2020-11-02 18:28:56 +0100

    Add support for OBJ groups

---
+++ src/main/java/malte0811/modelsplitter/model/Group.java
@@ -0,0 +1,73 @@
+package malte0811.modelsplitter.model;
+
+
+import com.google.common.collect.ImmutableList;
+import malte0811.modelsplitter.math.EpsilonMath;
+import malte0811.modelsplitter.math.Plane;
+import malte0811.modelsplitter.math.Vec3d;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+
+public class Group<Texture> {
+    private final List<Polygon<Texture>> faces;
+
+    public Group(List<Polygon<Texture>> faces) {
+        this.faces = ImmutableList.copyOf(faces);
+    }
+
+    public List<Polygon<Texture>> getFaces() {
+        return faces;
+    }
+
+    public Stream<Pair<EpsilonMath.Sign, Group<Texture>>> split(Plane p) {
+        Map<EpsilonMath.Sign, List<Polygon<Texture>>> splitFaces = new EnumMap<>(EpsilonMath.Sign.class);
+        for (Polygon<Texture> f : getFaces()) {
+            Map<EpsilonMath.Sign, Polygon<Texture>> splitResult = f.splitAlong(p);
+            for (Map.Entry<EpsilonMath.Sign, Polygon<Texture>> e : splitResult.entrySet()) {
+                splitFaces.computeIfAbsent(e.getKey(), s -> new ArrayList<>())
+                        .add(e.getValue());
+            }
+        }
+        return splitFaces.entrySet().stream()
+                .map(e -> Pair.of(e.getKey(), new Group<>(e.getValue())));
+    }
+
+    public Group<Texture> merge(Group<Texture> other) {
+        return new Group<>(
+                ImmutableList.<Polygon<Texture>>builder()
+                        .addAll(getFaces())
+                        .addAll(other.getFaces())
+                        .build()
+        );
+    }
+
+    private Group<Texture> mapFaces(UnaryOperator<Polygon<Texture>> map) {
+        return flatmapFaces(map.andThen(Stream::of));
+    }
+
+    private Group<Texture> flatmapFaces(Function<Polygon<Texture>, Stream<Polygon<Texture>>> map) {
+        return new Group<>(faces.stream()
+                .flatMap(map)
+                .collect(ImmutableList.toImmutableList())
+        );
+    }
+
+    public Group<Texture> translate(int axis, double amount) {
+        return mapFaces(p -> p.translate(axis, amount));
+    }
+
+    public Group<Texture> translate(Vec3d offset) {
+        return mapFaces(p -> p.translate(offset));
+    }
+
+    public Group<Texture> quadify() {
+        return flatmapFaces(p -> p.quadify().stream());
+    }
+}
--- src/main/java/malte0811/modelsplitter/model/OBJModel.java
+++ src/main/java/malte0811/modelsplitter/model/OBJModel.java
@@ -1,6 +1,8 @@
 package malte0811.modelsplitter.model;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import it.unimi.dsi.fastutil.doubles.DoubleArrays;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenCustomHashMap;
@@ -8,23 +10,59 @@ import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import malte0811.modelsplitter.math.EpsilonMath;
 import malte0811.modelsplitter.math.Plane;
 import malte0811.modelsplitter.math.Vec3d;
+import org.apache.commons.lang3.mutable.MutableObject;
+import org.apache.commons.lang3.tuple.Pair;
 
 import javax.annotation.Nullable;
 import java.io.*;
 import java.util.*;
+import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OBJModel<Texture> {
-    private final List<Polygon<Texture>> faces;
+    public static final String DEFAULT_GROUP = "default";
+    private final Map<String, Group<Texture>> faces;
+    private final List<Polygon<Texture>> allFaces;
 
+    @Deprecated
     public OBJModel(List<Polygon<Texture>> faces) {
-        this.faces = ImmutableList.copyOf(faces);
+        this(new Group<>(ImmutableList.copyOf(faces)));
+    }
+
+    @Deprecated
+    public OBJModel(Group<Texture> onlyGroup) {
+        this(ImmutableMap.of(DEFAULT_GROUP, onlyGroup));
+    }
+
+    public OBJModel(Map<String, Group<Texture>> faces) {
+        this.faces = ImmutableMap.copyOf(faces);
+        ImmutableList.Builder<Polygon<Texture>> faceListBuilder = ImmutableList.builder();
+        for (Group<Texture> group : faces.values()) {
+            Preconditions.checkState(!group.getFaces().isEmpty());
+            faceListBuilder.addAll(group.getFaces());
+        }
+        this.allFaces = faceListBuilder.build();
+    }
+
+    private OBJModel(Stream<Map.Entry<String, List<Polygon<Texture>>>> faces) {
+        this(
+                faces.map(e -> Pair.of(e.getKey(), new Group<>(e.getValue())))
+                        .collect(Collectors.toMap(Pair::getKey, Pair::getValue))
+        );
+    }
+
+    @Deprecated
+    public OBJModel() {
+        this(ImmutableList.of());
     }
 
     public static OBJModel<Void> readFromStream(InputStream source) {
         List<Vec3d> points = new ArrayList<>();
+        List<Vec3d> normals = new ArrayList<>();
         List<double[]> uvs = new ArrayList<>();
-        List<Polygon<Void>> faces = new ArrayList<>();
+        Map<String, List<Polygon<Void>>> groups = new HashMap<>();
+        MutableObject<String> currentGroup = new MutableObject<>(DEFAULT_GROUP);
         new BufferedReader(new InputStreamReader(source))
                 .lines()
                 .forEach(line -> {
@@ -36,6 +74,9 @@ public class OBJModel<Texture> {
                         case "v":
                             points.add(new Vec3d(readTokens(tokenizer, 3)));
                             break;
+                        case "vn":
+                            normals.add(new Vec3d(readTokens(tokenizer, 3)));
+                            break;
                         case "vt":
                             uvs.add(readTokens(tokenizer, 2));
                             break;
@@ -43,39 +84,51 @@ public class OBJModel<Texture> {
                             List<Vertex> vertices = new ArrayList<>();
                             while (tokenizer.hasMoreTokens()) {
                                 final String vertex = tokenizer.nextToken();
-                                final int slash = vertex.indexOf('/');
-                                int v = Integer.parseInt(vertex.substring(0, slash)) - 1;
-                                int vt = Integer.parseInt(vertex.substring(slash + 1)) - 1;
-                                vertices.add(new Vertex(
-                                        points.get(v),
-                                        //TODO
-                                        new Vec3d(0, 1, 0),
-                                        uvs.get(vt)
-                                ));
+                                String[] parts = vertex.split("/");
+                                int v = Integer.parseInt(parts[0]) - 1;
+                                double[] vertexUVs;
+                                if (!parts[1].isEmpty()) {
+                                    int vt = Integer.parseInt(parts[1]) - 1;
+                                    vertexUVs = uvs.get(vt);
+                                } else {
+                                    vertexUVs = new double[]{0, 0};
+                                }
+                                Vec3d normal;
+                                if (parts.length > 2) {
+                                    int vn = Integer.parseInt(parts[2]) - 1;
+                                    normal = normals.get(vn);
+                                } else {
+                                    normal = new Vec3d(0, 1, 0);
+                                }
+                                vertices.add(new Vertex(points.get(v), normal, vertexUVs));
                             }
-                            faces.add(new Polygon<>(vertices, null));
+                            groups.computeIfAbsent(currentGroup.getValue(), s -> new ArrayList<>())
+                                    .add(new Polygon<>(vertices, null));
+                            break;
+                        case "o":
+                            currentGroup.setValue(tokenizer.nextToken());
                             break;
                         default:
                             System.out.println("Ignoring line " + line);
                             break;
                     }
                 });
-        return new OBJModel<>(faces);
-    }
-
-    public OBJModel() {
-        this(ImmutableList.of());
+        return new OBJModel<>(groups.entrySet().stream());
     }
 
     public static <Texture> OBJModel<Texture> union(@Nullable OBJModel<Texture> a, @Nullable OBJModel<Texture> b) {
-        List<Polygon<Texture>> result = new ArrayList<>();
+        Stream<Map.Entry<String, Group<Texture>>> unionGroups = Stream.empty();
         if (a != null) {
-            result.addAll(a.getFaces());
+            unionGroups = Stream.concat(unionGroups, a.getFacesByGroup().entrySet().stream());
         }
         if (b != null) {
-            result.addAll(b.getFaces());
+            unionGroups = Stream.concat(unionGroups, b.getFacesByGroup().entrySet().stream());
         }
-        return new OBJModel<>(result);
+        return new OBJModel<>(unionGroups.collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                Group::merge
+        )));
     }
 
     private static double[] readTokens(StringTokenizer tokenizer, int tokens) {
@@ -86,14 +139,13 @@ public class OBJModel<Texture> {
         return data;
     }
 
-    public Map<EpsilonMath.Sign, OBJModel<Texture>> split(Plane p) {
-        Map<EpsilonMath.Sign, List<Polygon<Texture>>> resultFaces = new EnumMap<>(EpsilonMath.Sign.class);
-        for (Polygon<Texture> f : this.faces) {
-            Map<EpsilonMath.Sign, Polygon<Texture>> splitResult = f.splitAlong(p);
-            for (Map.Entry<EpsilonMath.Sign, Polygon<Texture>> e : splitResult.entrySet()) {
-                resultFaces.computeIfAbsent(e.getKey(), s -> new ArrayList<>())
-                        .add(e.getValue());
-            }
+    public Map<EpsilonMath.Sign, OBJModel<Texture>> split(Plane splitPlane) {
+        Map<EpsilonMath.Sign, Map<String, Group<Texture>>> resultFaces = new EnumMap<>(EpsilonMath.Sign.class);
+        for (Map.Entry<String, Group<Texture>> group : this.faces.entrySet()) {
+            group.getValue().split(splitPlane).forEach(
+                    p -> resultFaces.computeIfAbsent(p.getKey(), s -> new HashMap<>())
+                            .merge(group.getKey(), p.getValue(), Group::merge)
+            );
         }
         return resultFaces.entrySet()
                 .stream()
@@ -104,20 +156,23 @@ public class OBJModel<Texture> {
         PrintStream out = new PrintStream(outRaw);
         Object2IntMap<Vec3d> points = new Object2IntOpenHashMap<>();
         Object2IntMap<double[]> uvs = new Object2IntOpenCustomHashMap<>(DoubleArrays.HASH_STRATEGY);
-        for (Polygon<Texture> f : faces) {
-            StringJoiner line = new StringJoiner(" ", "f ", "");
-            for (Vertex v : f.getPoints()) {
-                final int vIndex = points.computeIntIfAbsent(v.getPosition(), pos -> {
-                    out.printf("v %.4f %.4f %.4f\n", pos.get(0), pos.get(1), pos.get(2));
-                    return points.size();
-                }) + 1;
-                final int uvIndex = uvs.computeIntIfAbsent(v.getUV(), uv -> {
-                    out.printf("vt %.6f %.6f\n", uv[0], uv[1]);
-                    return uvs.size();
-                }) + 1;
-                line.add(vIndex + "/" + uvIndex);
+        for (Map.Entry<String, Group<Texture>> group : faces.entrySet()) {
+            out.println("o " + group.getKey());
+            for (Polygon<Texture> f : group.getValue().getFaces()) {
+                StringJoiner line = new StringJoiner(" ", "f ", "");
+                for (Vertex v : f.getPoints()) {
+                    final int vIndex = points.computeIntIfAbsent(v.getPosition(), pos -> {
+                        out.printf("v %.4f %.4f %.4f\n", pos.get(0), pos.get(1), pos.get(2));
+                        return points.size();
+                    }) + 1;
+                    final int uvIndex = uvs.computeIntIfAbsent(v.getUV(), uv -> {
+                        out.printf("vt %.6f %.6f\n", uv[0], uv[1]);
+                        return uvs.size();
+                    }) + 1;
+                    line.add(vIndex + "/" + uvIndex);
+                }
+                out.println(line.toString());
             }
-            out.println(line.toString());
         }
     }
 
@@ -126,30 +181,30 @@ public class OBJModel<Texture> {
     }
 
     public List<Polygon<Texture>> getFaces() {
+        return allFaces;
+    }
+
+    public Map<String, Group<Texture>> getFacesByGroup() {
         return faces;
     }
 
-    public OBJModel<Texture> translate(int axis, double amount) {
-        List<Polygon<Texture>> translatedFaces = new ArrayList<>(faces.size());
-        for (Polygon<Texture> p : faces) {
-            translatedFaces.add(p.translate(axis, amount));
+    private OBJModel<Texture> mapGroups(UnaryOperator<Group<Texture>> map) {
+        Map<String, Group<Texture>> translatedGroups = new HashMap<>();
+        for (Map.Entry<String, Group<Texture>> p : faces.entrySet()) {
+            translatedGroups.put(p.getKey(), map.apply(p.getValue()));
         }
-        return new OBJModel<>(translatedFaces);
+        return new OBJModel<>(translatedGroups);
+    }
+
+    public OBJModel<Texture> translate(int axis, double amount) {
+        return mapGroups(g -> g.translate(axis, amount));
     }
 
     public OBJModel<Texture> translate(Vec3d offset) {
-        List<Polygon<Texture>> translatedFaces = new ArrayList<>(faces.size());
-        for (Polygon<Texture> p : faces) {
-            translatedFaces.add(p.translate(offset));
-        }
-        return new OBJModel<>(translatedFaces);
+        return mapGroups(g -> g.translate(offset));
     }
 
     public OBJModel<Texture> quadify() {
-        List<Polygon<Texture>> translatedFaces = new ArrayList<>(faces.size());
-        for (Polygon<Texture> p : faces) {
-            translatedFaces.addAll(p.quadify());
-        }
-        return new OBJModel<>(translatedFaces);
+        return mapGroups(Group::quadify);
     }
 }
